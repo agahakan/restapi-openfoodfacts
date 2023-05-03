c86c07a58a09a1028a89432384494e92dcc2622d
agahakan <qirahakan@gmail.com>
2023-05-03 22:46:10 +0200

    add: authentification system with prisma

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards, Patch } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { GetUser } from "src/auth/decorator";
+import { JwtGuard } from "src/auth/guard";
+
+@UseGuards(JwtGuard)
+@Controller("users")
+export class UserController {
+  @Get("me")
+  getMe(@GetUser("id") user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {}
+}
